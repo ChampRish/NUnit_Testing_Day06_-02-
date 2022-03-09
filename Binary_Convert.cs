1a89e0811b28220da2a7e5e59534e6c98d8e9fd4
RISHABH <gguptarishabh82@gmail.com>
2022-03-09 13:17:48 +0530

    Convert to binary

---
+++ Binary_Convert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs_For_NUnit_Testing_Day06_02
+{
+    public class Binary_Convert
+    {
+        public static void toBinary()
+        {
+            Console.WriteLine("Enter to Number : ");
+            int value = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine(binary);
+        }
+    }
+}
