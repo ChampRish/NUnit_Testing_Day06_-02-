9a2d6ffba88e24b972222bfde2c57ba8e7da6f09
RISHABH <gguptarishabh82@gmail.com>
2022-03-09 13:10:13 +0530

    Program for Nunit Testing

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programs_For_NUnit_Testing_Day06_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureConversion.DisplayTempConversion();
+            Vending_Machine.countCurrency();
+            Monthly_Payment.CalculateMonthlyPayment();
+            Binary_Convert.toBinary();
+
+            Console.ReadLine();
+        }
+    }
+}
